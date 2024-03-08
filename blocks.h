94faed70551e1cc1531fdd6cc75d984f630398db
rylenko <rylenko@tuta.io>
2024-03-08 15:54:11 +0700

    Update block names

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@ static const Block blocks[] = {
 	/* Icon, command, update interval and update signal */
 	{"", "smpc-block", 1, 0},
 	{"", "wttr-block", 60, 0},
-	{"", "pkg-info-count-block", 15, 0},
+	{"", "pkg-info-cnt-block", 15, 0},
 	{"", "ram-block", 5, 0},
 	{"", "apm-block", 5, 0},
 	{"", "sndio-block", 5, 1},
-	{"", "datetime-block", 20, 0},
+	{"", "dt-block", 20, 0},
 };
 
 static char delim[] = " ";
